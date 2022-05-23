fe2abf9ce3bf03ffdfbf058319b8eefc8d031fda
David Geraldo <davidg@hanabank.co.id>
2022-05-23 15:54:34 +0700

    my-app-config.ts

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa55a9znndmkROu15d7',
+        issuer: 'https://dev-96815186.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
