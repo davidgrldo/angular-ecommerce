1dec579856e7302d657fd5e732dfd4265741deb4
David Geraldo <davidg@hanabank.co.id>
2022-05-24 12:08:09 +0700

    release 3.0 - login/logout with okta

---
+++ src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent implements OnInit {
+
+  isAuthenticated: boolean = false;
+  userFullName: string;
+  
+  constructor(private oktaAuthService: OktaAuthService) { }
+
+  ngOnInit(): void {
+    // Subscribe to authentication state changes
+    this.oktaAuthService.$authenticationState.subscribe(
+      (result) => {
+        this.isAuthenticated = result;
+        this.getUserDetails();
+      }
+    );
+  }
+
+  getUserDetails() {
+    /**
+     * fetch the logged in user details (user's claim)
+     * user full name is exposed as a property name
+     */
+    if (this.isAuthenticated) {
+      this.oktaAuthService.getUser().then(
+        (res) => {
+          this.userFullName = res.name;
+        }
+      );
+    }
+  }
+
+  logout() {
+    // terminates the session with Okta and removes current tokens.
+    this.oktaAuthService.signOut();
+  }
+
+}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {OktaAuth} from '@okta/okta-angular';
+import { OktaAuthService } from '@okta/okta-angular';
+import * as OktaSignIn from '@okta/okta-signin-widget';
 
 import myAppConfig from '../../config/my-app-config';
 
@@ -10,11 +11,35 @@ import myAppConfig from '../../config/my-app-config';
 })
 export class LoginComponent implements OnInit {
 
-  octaSign: any;
+  oktaSignin: any;
 
-  constructor(private oktaAuthService: OktaAuthService) { }
+  constructor(private oktaAuthService: OktaAuthService) {
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+  }
 
   ngOnInit(): void {
-  }
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({
+      el: '#octa-sign-in-widget' // this name should be same as div tag id in login.component.html
+    }, (response) => {
+      if (response.status === 'SUCCESS') {
+        this.oktaAuthService.signInWithRedirect();
+      }
+    }, (error) => {
+      throw error;
+    }
+  )
+}
 
 }
